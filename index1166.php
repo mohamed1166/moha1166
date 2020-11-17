0b7d01398002c4fcc982e513b98b8f04a9cb1229
mohamed1166 <74587216+mohamed1166@users.noreply.github.com>
2020-11-17 09:13:26 +0200

    Add files via upload

---
+++ index1166.php
@@ -0,0 +1,38 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>mohamed</title>
+</head>
+<body bgcolor="gereen">
+
+<center>
+<h1>بسم الله الرحمن الرحيم</h1>
+<br>
+<h3>كلية الامارات للعلوم والتكنولوجيا </h3>
+<h2>قسم تقانه المعلومات<h2>
+<?php
+$name="mohamed haroun";
+$class="IT 3";
+$subject="PHP";
+$addess="khartoum";
+$ID_student="10767483";
+
+echo"$name"."<br>";
+echo"<br>";
+echo"class:"."$class"."<br>";
+echo"<br>";
+echo"subject:"."$subject"."<br>";
+echo"<br>";
+echo"addess:"."$addess"."<br>";
+echo"<br>";
+echo"ID_student:"."$ID_student"."<br>";
+echo"<br>";
+
+
+?>
+
+</center>
+</body>
+</html>
